682b8266933a2a515578deebd2b985f88aeecbab
Alex Su <alexanderytsu@gmail.com>
2019-10-22 21:58:23 +1100

    (webpack): make webpack digest css files properly

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { observable } from "mobx";
-import { observer } from "mobx-react";
+
+import App from "./App";
+
 
 import "./ui/tailwind.css";
-
-class AppState {
-	@observable timer = 0;
-
-	constructor() {
-		setInterval(() => {
-			this.timer += 1;
-		}, 1000);
-	}
-
-	resetTimer() {
-		this.timer = 0;
-	}
-}
-
-@observer
-class TimerView extends React.Component<{ appState: AppState }, {}> {
-	render() {
-		return (
-			<div>
-				<button onClick={this.onReset}>
-					Seconds passed: {this.props.appState.timer}
-				</button>
-			</div>
-		);
-	}
-
-	onReset = () => {
-		this.props.appState.resetTimer();
-	};
-}
-
-const appState = new AppState();
 ReactDOM.render(
-	<TimerView appState={appState} />,
+	<App></App>,
 	document.getElementById("root")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,41 @@
 var path = require("path");
 var webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	devtool: "eval",
-	mode: "development",
-	entry: ["webpack-dev-server/client?http://localhost:3000", "./src/index"],
-	output: {
-		path: path.join(__dirname, "dist"),
-		filename: "bundle.js",
-		publicPath: "/static/"
-	},
-	resolve: {
-		extensions: [".js", ".ts", ".tsx"]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				loader: "awesome-typescript-loader",
-				include: path.join(__dirname, "src")
-			}
-		]
-	}
+  devtool: "eval",
+  mode: "development",
+  entry: ["webpack-dev-server/client?http://localhost:3000", "./src/index"],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/static/"
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        include: path.join(__dirname, "src")
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+      chunkFilename: "styles.css"
+    }),
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+      filename: "./index.html"
+    })
+  ]
 };
