88ef67bd5c0cd787145cba17641f8a4862e18797
Alex Su <alexanderytsu@gmail.com>
2019-10-23 14:55:02 +1100

    (format) linted createcategory

--- src/ui/components/category/CreateCategory.tsx
+++ src/ui/components/category/CreateCategory.tsx
@@ -1,51 +1,50 @@
 import * as React from "react";
-import {Component} from "react";
+import { Component } from "react";
 
 import "../../tailwind.css";
 import { ENETDOWN } from "constants";
 import apiHelpers from "../../../util/api-helpers";
 
 interface CreateCategoryState {
-  category: string;
+	category: string;
 }
 
-export default class CreateCategory extends Component<{},CreateCategoryState> {
+export default class CreateCategory extends Component<{}, CreateCategoryState> {
+	constructor(props) {
+		super(props);
 
-  constructor(props) {
-    super(props);
+		this.state = {
+			category: ""
+		};
+	}
 
-    this.state = {
-      category: "",
-    };
+	onInputChanged = event => {
+		this.setState({ category: event.target.value });
+	};
 
-  }
+	submit = async () => {
+		await apiHelpers.loginUser("joe", "password");
+		await apiHelpers.createCategory(this.state.category);
+	};
 
-  onInputChanged = (event) => {
-    this.setState({category: event.target.value});
-  }
-
-  submit = async () => {
-    await apiHelpers.loginUser("joe", "password");
-    await apiHelpers.createCategory(this.state.category);
-  }
-
-  render() {
-    return (
-      <div className="bg-white rounded-lg shadow-md p-8">
-        <label htmlFor="category">Category Name</label>
-        <input
-          id="category"
-          placeholder="Category"
-          value={this.state.category}
-          onChange={(event)=>this.onInputChanged(event)}
-          className="mb-8"
-        />
-        <button 
-        onClick={this.submit}
-        className="bg-teal-500 text-white py-2 px-4 rounded">Create Category
-        </button>
-      </div>
-    )
-  }
-
-} 
+	render() {
+		return (
+			<div className="bg-white rounded-lg shadow-md p-8">
+				<label htmlFor="category">Category Name</label>
+				<input
+					id="category"
+					placeholder="Category"
+					value={this.state.category}
+					onChange={event => this.onInputChanged(event)}
+					className="mb-8"
+				/>
+				<button
+					onClick={this.submit}
+					className="bg-teal-500 text-white py-2 px-4 rounded"
+				>
+					Create Category
+				</button>
+			</div>
+		);
+	}
+}
