593d54c71a466a3f01fdc403fa51aaf61171784f
Alex Su <alexanderytsu@gmail.com>
2019-11-05 00:15:08 +1100

    (ledger): support transaction deletion

--- src/ui/scenes/LedgerScene.tsx
+++ src/ui/scenes/LedgerScene.tsx
@@ -3,27 +3,70 @@ import { Component } from "react";
 import { observer } from "mobx-react";
 
 import apiHelpers from "../../util/api-helpers";
-import UserStore from "../../stores/UserStore";
 import ApplicationStore from "../../stores/ApplicationStore";
-import { TransactionDisplayProps } from "../../util/types/TransactionTypes";
 import Transaction from "../components/transaction/Transaction";
 import TransactionForm from "../components/transaction/TransactionForm";
+import moment = require("moment");
+import { number } from "prop-types";
+import { triggerAsyncId } from "async_hooks";
+import UserStore from "../../stores/UserStore";
+
+interface LedgerSceneState {
+	selectedTransactionId: number;
+}
 
 @observer
-export default class LedgerScene extends Component {
+export default class LedgerScene extends Component<{}, LedgerSceneState> {
+	constructor(props) {
+		super(props);
+		this.state = { selectedTransactionId: 0 };
+	}
+
 	render() {
 		return (
 			<div className="flex">
 				<div className="w-full mr-4">
 					<h1 className="text-xl">Ledger</h1>
-					{ApplicationStore.transactions_raw.map(tr_raw => {
-						const tr = apiHelpers.convertTransaction(tr_raw);
-						return (
-							<div className="my-2">
-								<Transaction key={tr.id} {...tr}></Transaction>
-							</div>
-						);
-					})}
+					{ApplicationStore.transactions_raw
+						.sort((a, b) => {
+							return moment(b.date).diff(a.date);
+						})
+						.map(tr_raw => {
+							const tr = apiHelpers.convertTransaction(tr_raw);
+
+							if (tr.id === this.state.selectedTransactionId) {
+								return (
+									<div key={tr.id} className="my-2 bg-red-300 p-2 rounded-lg">
+										<Transaction {...tr}></Transaction>
+									</div>
+								);
+							}
+
+							return (
+								<div
+									key={tr.id}
+									onClick={() => {
+										this.setState({ selectedTransactionId: tr.id });
+									}}
+									className="my-2"
+								>
+									<Transaction {...tr}></Transaction>
+								</div>
+							);
+						})}
+					{this.state.selectedTransactionId !== 0 ? (
+						<button
+							className="rounded bg-red-600 text-white p-2 shadow"
+							onClick={() => {
+								ApplicationStore.deleteTransaction(
+									UserStore.token,
+									this.state.selectedTransactionId
+								);
+							}}
+						>
+							Delete
+						</button>
+					) : null}
 				</div>
 				<div className="w-full ml-4">
 					<h1 className="text-xl">New Transaction</h1>
